6c293e28f7fa10fc6d4fa55b53983434ce9eb299
maryko-makk <maryko-makk@yandex.ru>
2021-03-06 18:17:45 +0300

    add function body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ for (let i=0; i<bracketsConfig.length; i++) {
+ let sequence = bracketsConfig[i].join('');
+ if (str.includes(sequence)) {
+   str = str.replace(sequence, '');
+   i = -1;
+  }
+}
+  return (str.length == 0);
 }
